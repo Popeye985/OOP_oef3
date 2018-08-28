7f9312f06bba53a15135a18172344b3a2f132438
Popeye985 <ceulemans_koen@hotmail.com>
2018-08-28 16:18:19 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,50 @@
+package com.company;
+
+import java.util.Objects;
+
+public class Main {
+
+
+    // write your code here
+    public static void main(String[] args) {
+        Zangeres karen = new Zangeres("Karen", "Damen");
+        System.out.println(karen);
+    }
+}
+
+class Zangeres extends BasisPersoon {
+    public Zangeres(String voornaam, String achternaam) {
+        super(voornaam, achternaam);
+
+
+    }
+    @Override
+    public String toString() {
+        return String.format("Deze zangeres heet %s %s", getVoornaam(), getAchternaam());
+    }
+
+}
+
+class BasisPersoon{
+    private String voornaam;
+    private String achternaam;
+
+    public BasisPersoon(String voornaam, String achternaam) {
+        this.voornaam = voornaam;
+        this.achternaam = achternaam;
+    }
+
+    public String getVoornaam() {
+        return voornaam;
+    }
+
+    public String getAchternaam() {
+        return achternaam;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Deze persoon heet %s %s", voornaam, achternaam);
+    }
+}
+
